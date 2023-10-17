f519b97089fd04d46053f22a469af2ce4acc9433
Jocelyn <cadesigner552@gmail.com>
2023-10-17 07:26:33 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const right = document.querySelector('.navbar_right');
 const menu = document.querySelector('.navbar_menu');
 const icons = document.querySelector('.navbar_icons');
 
 toggleBtn. addEventListener('click', () => {
+    right.classList.toggle('active');
     menu.classList.toggle('active');
     icons.classList.toggle('active');
 });
